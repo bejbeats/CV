3bcab8ea3807212a817e6893144e0071aca60f79
bejbeats <106589810+bejbeats@users.noreply.github.com>
2022-08-29 21:25:17 +0200

    Add files via upload

---
+++ slicksettings.js
@@ -0,0 +1,20 @@
+$(".team-carousel").slick({
+  arrows: false,
+  autoplay: true,
+  infinite: true,
+  mobileFirst: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplaySpeed: 500,
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: { slidesToShow: 2 },
+    },
+    {
+      breakpoint: 992,
+      settings: { slidesToShow: 3 },
+    },
+  ],
+});
